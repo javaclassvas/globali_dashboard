5b444c7b7dce380c4517ba215b937f3b1e602cc9
Vasily Alexeev <javaclassvas@gmail.com>
2020-08-01 22:34:38 +0300

    footer design 1

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,11 +5,21 @@ import styles from './/footer.module.scss';
 const Footer = () => {
     return (
         <div className={styles.footer}>
-            <h2>FOOTER</h2>
+            <div className={styles.cont}>
+                <div className={styles.left}>
+                    <h2>LEFTSIDE</h2>
+                </div>
+                <div className={styles.center}>
+                    <h2>CENTER</h2>
+                </div>
+                <div className={styles.right}>
+                    <h2>RIGHTSIDE</h2>
+                </div>
+            </div>
+
         </div>
     )
 }
-
 
 
 export default Footer;
